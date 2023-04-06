55ac606e04d3aa03526338413eb2f50192357fec
yashwalker7 <102256756+yashwalker7@users.noreply.github.com>
2023-04-06 18:02:33 +0530

    Create Equal Left and Right Subarray Sum.py

---
+++ GFG/Equal Left and Right Subarray Sum.py
@@ -0,0 +1,47 @@
+# User function Template for python3
+
+class Solution:
+    # Complete this function
+    
+    #Function to find equilibrium point in the array.
+    def equalSum(self,A, N):
+        # Your code here
+        l_sum = 0
+        r_sum = sum(A)
+        for i in range(1, N+1):
+            r_sum -= A[i-1]
+            if l_sum == r_sum:
+                return i
+            l_sum += A[i-1]
+        
+        return -1
+
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+import math
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+
+        N = int(input())
+
+        A = [int(x) for x in input().strip().split()]
+        ob=Solution()
+
+        print(ob.equalSum(A, N))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
